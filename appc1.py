0a6c8d6e81f0a52fcdcb85c8a3fd80b3383e6eb6
nguyenphuonganhhhhhhh-tech <nguyenphuonganhhhhhhh@gmail.com>
2026-07-05 23:52:22 +0700

    Update appc1.py

--- appc1.py
+++ appc1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Thiết lập cấu hình trang hiển thị trực quan
-st.set_page_config(page_title="Kiểm tra Quang sinh học", page_icon="☀️", layout="centered")
+st.set_page_config(page_title="CHƯƠNG 5", page_icon="☀️", layout="centered")
 
 # --- DỮ LIỆU PHẦN I: 100 CÂU HỎI TRẮC NGHIỆM ---
 quiz_data = [
